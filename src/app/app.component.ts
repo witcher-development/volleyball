98b1f93359b1c027051d40acde5638084ccec379
witcher-reqestum <roman.web.kuznietsov@gmail.com>
2020-01-08 17:46:38 +0200

    continue work on model

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,15 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Team } from '../model';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'volleyball';
+
+  ngOnInit(): void {
+    const team = new Team();
+  }
 }
--- src/model/index.ts
+++ src/model/index.ts
@@ -1,16 +1,36 @@
-export type PlayerSkillI = 1 | 2 | 3;
-export type PlayerIdI = number;
+import * as uuid from 'uuid';
+import { getRandomInt } from '../helpers';
 
-export interface PlayerI {
-  id: PlayerIdI;
+export type PlayerSkillI = 1 | 2 | 3 | 0;
+export type PlayerIdI = string;
+export type PlayerPositionI = 1 | 2 | 3 | 4 | 5 | 6;
+export interface PlayerInfo {
+  position: PlayerPositionI;
   skill: PlayerSkillI;
 }
 
-export type TeamIdI = number;
+export interface PlayerI {
+  id: PlayerIdI;
+  position: PlayerPositionI;
+  skill: PlayerSkillI;
+
+  makeShoot(direction: PlayerIdI, power: ShootPowerI, angle: ShootAngleI): void;
+}
+
+export type TeamIdI = string;
+export type TeamNameI = string;
 
 export interface TeamI {
   id: TeamIdI;
+  name: TeamNameI;
   players: PlayerI[];
+
+  score: number;
+
+  setPlayer(position: PlayerPositionI, player: PlayerI): void;
+  setTeam(players: PlayerInfo[]): void;
+
+  randomTeam(): void;
 }
 
 export interface FieldI {
@@ -45,13 +65,74 @@ export interface GameI {
   rounds: RoundI[];
   currentRoundId: RoundIdI;
 
-  initTeams(): void;
+  initGame(teams: TeamI[]): void;
+  initRound(startingTeam: TeamNameI): void;
+}
+
+// export class Shoot implements ShootI {}
+
+export class Player implements PlayerI {
+  public id = uuid();
+  public position;
+  public skill;
+
+  constructor(position, skill) {
+    this.position = position;
+    this.skill = skill;
+  }
+
+  makeShoot(direction, power, angle) {}
+}
+
+export class Team implements TeamI {
+  public id = uuid();
+  public name;
+  public players = [];
+  public score = 0;
+
+  constructor(teamName = 'DeepMind') {
+    this.name = teamName;
+  }
+
+  setPlayer(position, skill) {
+    this.players[position - 1] = new Player(position, skill);
+  }
+  setTeam(players: PlayerInfo[]): void {
+    players.forEach(({ position, skill }) => {
+      this.setPlayer(position, skill);
+    });
+  }
+  randomTeam(): void {
+    const skillCounter = [0, 0, 0];
+
+    const getRandomSkillLevel = (): PlayerSkillI => {
+      let skill = getRandomInt(3) as PlayerSkillI;
+
+      if (skillCounter[skill - 1] === 2) {
+        skill = getRandomSkillLevel();
+      } else {
+        skillCounter[skill - 1]++;
+      }
+
+      return skill;
+    };
+
+    for (let i = 1; i < 7; i++) {
+      this.setPlayer(i as PlayerPositionI, getRandomSkillLevel());
+    }
+  }
 }
 
 export class Game implements GameI {
-  constructor() {
-    // this.field = this.initTeams();
-  }
+  public field;
+  public rounds;
+  public currentRoundId;
 
-  initTeams() {}
+  initGame(teams) {
+    this.field[teams[0].name] = teams[0];
+    this.field[teams[1].name] = teams[1];
+
+    this.initRound(teams[0].name);
+  }
+  initRound(startingTeam) {}
 }
